607a4f2d65dbf26788b25620fe514ec96016c0e4
Martunis <martunismukhtar@gmail.com>
2020-12-04 22:00:34 +0700

    app

--- app/Mail/SendEmail.php
+++ app/Mail/SendEmail.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class SendEmailTest extends Mailable
+class SendEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
